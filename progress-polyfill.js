b320904c90fb0941ff8b0e53c635da479eb19aa8
Laurent Goderre <laurent.goderre@gmail.com>
2012-08-09 11:47:17 -0400

    Converted code to full jQuery, JSLinted the code and added support for IE7

--- progress-polyfill.js
+++ progress-polyfill.js
@@ -1,33 +1,30 @@
-$(function(){
-  document.createElement('progress');
-  var updatePolyfill = function(progressElem, progressBarDiv) {
-    var params = $([($(progressElem).attr('max') || '1.0'), $(progressElem).attr('value')]).map(function() {
-      if (/^\-?\d+(?:\.\d+)?$/.test(this)) {
-	return parseFloat(this);
-      }
-    }).get();
-    var max = params[0];
-    var val = params[1];
-    if (val > max) val = max;
-    var amt = (val / max) * 100.0;
-    progressBarDiv.style.width = amt + "%";
-  };
-  $('progress[value]').each(function(index) {
-    var progressFrameDiv = document.createElement('div');
-    var progressBarDiv = document.createElement('div');
-    progressFrameDiv.appendChild(progressBarDiv);
-    $(progressFrameDiv).addClass('progress-frame');
-    $(progressBarDiv).addClass('progress-bar');
-    updatePolyfill(this, progressBarDiv);
-    this.appendChild(progressFrameDiv);
-    $(this).bind("DOMAttrModified", function(event) {
-      updatePolyfill(this, progressBarDiv);
-    });
-  });
+(function ($) {
+	document.createElement('progress');
+	var updatePolyfill = function (progressElem, progressBarDiv) {
+		var params = $([(progressElem.attr('max') || '1.0'), progressElem.attr('value')]).map(function () {
+			if (/^\-?\d+(?:\.\d+)?$/.test(this)) {
+				return parseFloat(this);
+			}
+		}).get(),
+			max = params[0],
+			val = params[1],
+			amt;
 
-  $('progress:not([value])').each(function(index) {
-    var progressDiv = document.createElement('div');
-    $(progressDiv).addClass('progress-undefined');
-    this.appendChild(progressDiv);
-  });
-});
+		if (val > max) {val = max; }
+		amt = (val / max) * 100.0;
+		progressBarDiv.css("width", amt + "%");
+	};
+	$('progress[value]').each(function (index) {
+		var progressDiv = $('<div class="progress-frame"><div class="progress-bar"/></div>');
+		$(this).append(progressDiv);
+		$(this).on("DOMAttrModified propertychange", function (event) {
+			updatePolyfill($(this), progressDiv.children('.progress-bar'));
+		});
+		$(this).trigger('DOMAttrModified');
+	});
+
+	$('progress:not([value])').each(function (index) {
+		var progressDiv = $('<div class="progress-undefined"/>');
+		$(this).append(progressDiv);
+	});
+}(jQuery));
